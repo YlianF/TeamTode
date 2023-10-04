c0bc69b7fcb3cfc0f345dfca14f3c9e35c362e51
Ylian Fatmi <yli.fatmi@gmail.com>
2023-10-04 16:30:17 +0200

    ajout admin dashboard

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
