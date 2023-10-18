971b253b0dfce5aaacd5afb9dc6d28729a56ccd6
Ylian Fatmi <yli.fatmi@gmail.com>
2023-10-18 15:46:39 +0200

    interface

--- src/Controller/TimtodeController.php
+++ src/Controller/TimtodeController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Annonces;
+use Symfony\Component\HttpFoundation\Request;
 use App\Repository\AnnoncesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,11 +13,20 @@ class TimtodeController extends AbstractController
 {
     public int $ID_JEU = 1;
     #[Route('/timtode', name: 'app_timtode')]
-    public function index(Environment $twig, AnnoncesRepository $AnnoncesRepository): Response
+    public function index(AnnoncesRepository $AnnoncesRepository): Response
     {
         return $this->render('timtode/index.html.twig', [
             'controller_name' => 'TimtodeController',
             'annonces' => $AnnoncesRepository->findBy(['jeu' => $this->ID_JEU], ['date' => 'DESC']),
+        ]);
+    }
+
+    #[Route('/timtode/annonce/{id}', name: 'app_timtode_annonce')]
+    public function show(Environment $twig, Annonces $annonce): Response
+    {
+        return $this->render('timtode/annonce.html.twig', [
+            'controller_name' => 'TimtodeController',
+            'annonce' => $annonce,
         ]);
     }
 }
