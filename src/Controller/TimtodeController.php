b495953c43b304ae6e35431d5f889c0754ffbcc5
Ylian Fatmi <yli.fatmi@gmail.com>
2023-10-19 16:38:59 +0200

    ajout form commentaires

--- src/Controller/TimtodeController.php
+++ src/Controller/TimtodeController.php
@@ -1,9 +1,14 @@
 <?php
 
 namespace App\Controller;
+
 use App\Entity\Annonces;
+use App\Entity\Comment;
+
+use App\Form\CommentFormType;
 use Symfony\Component\HttpFoundation\Request;
 use App\Repository\AnnoncesRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,11 +27,26 @@ class TimtodeController extends AbstractController
     }
 
     #[Route('/timtode/annonce/{id}', name: 'app_timtode_annonce')]
-    public function show(Environment $twig, Annonces $annonce): Response
+    public function show(Request $request, Environment $twig, Annonces $annonce, EntityManagerInterface $entityManager): Response
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment = $form->getData();
+
+            $comment->setAnnonce($annonce);
+            $comment->setAuthor($this->getUser());
+
+            $entityManager->persist($comment);
+            $entityManager->flush();
+        }
+
         return $this->render('timtode/annonce.html.twig', [
             'controller_name' => 'TimtodeController',
             'annonce' => $annonce,
+            'form' => $form,
         ]);
     }
 }
